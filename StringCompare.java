e8999cf1a322d26fb63755847b0587a239d539ce
Mehdi83 <mehdy1982_me@yahoo.com>
2022-10-05 23:08:59 +0300

    Equal terms added

--- StringCompare.java
+++ StringCompare.java
@@ -1,11 +1,13 @@
 public class StringCompare {
-    public static void main(String[] args) {
-    }
-
+    public static void main(String[] args) {}
     public static boolean compareStr(String Str1, String Str2) {
 
         if (Str1.length() > Str2.length()) {
             return true;
+        }
+
+        else if (Str2.length() == Str2.length()) {
+            System.out.println("Equal strings length");
         }
         return false;
     }
