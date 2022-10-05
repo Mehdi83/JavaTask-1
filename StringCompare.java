f40287272cccf6e6854c1355e8ba23457575688f
MariaBut <112708831+MariaBut@users.noreply.github.com>
2022-10-05 23:03:41 +0300

    Initial commit

---
+++ StringCompare.java
@@ -0,0 +1,12 @@
+public class StringCompare {
+    public static void main(String[] args) {
+    }
+
+    public static boolean compareStr(String Str1, String Str2) {
+
+        if (Str1.length() > Str2.length()) {
+            return true;
+        }
+        return false;
+    }
+}
